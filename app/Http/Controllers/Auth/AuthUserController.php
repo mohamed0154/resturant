99e55e1e66cf9fe75adba4be6d0bfd0ecc311c57
mohamed0154 <mf5967482@gmail.com>
2025-06-08 18:45:53 +0300

    make dendency ingection

--- app/Http/Controllers/Auth/AuthUserController.php
+++ app/Http/Controllers/Auth/AuthUserController.php
@@ -5,11 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
-use App\Models\User;
 use App\services\AuthenticationServices;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class AuthUserController extends Controller
 {
--- app/Http/Controllers/Dishes/SearchController.php
+++ app/Http/Controllers/Dishes/SearchController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Dishes;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SearchRequest;
-use App\Models\Dish;
 use App\services\SearchServices;
-use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
